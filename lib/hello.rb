ca9a695146bc26083fac3e1e910ccf0cf807605d
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-13 15:06:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,9 @@
 def hello_t(array)
-  if block_given?
   i= 0 
   
-  while 1 < array.length
-    yield (array [i])
-   i= i + 1 
-  end 
- 
-  array
-else 
-  puts "Hey! No block was given!"
-  end 
+  while i < array.length 
+  i = i + 1 
+  end
 end 
 
 # call your method here!
